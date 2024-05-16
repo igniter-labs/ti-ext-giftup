80166d3cc9b810b08b7aafa431b6aa8ecbe1039b
Rukmi <rukmi.patel@itreeni.com>
2024-05-16 19:18:44 +0530

    GiftUp: Apply GiftUp Value based on cart conditions from admin settings.

--- cartconditions/RedeemGiftCard.php
+++ cartconditions/RedeemGiftCard.php
@@ -2,8 +2,10 @@
 
 namespace IgniterLabs\GiftUp\CartConditions;
 
-use Igniter\Flame\Cart\CartCondition;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Igniter\Flame\Cart\Facades\Cart;
+use Igniter\Flame\Cart\CartCondition;
 use IgniterLabs\GiftUp\Classes\Manager;
 
 class RedeemGiftCard extends CartCondition
@@ -13,6 +15,8 @@ class RedeemGiftCard extends CartCondition
     public $priority = 200;
 
     protected $giftCardValue = 0;
+
+    public $conditions = null;
 
     public function getLabel()
     {
@@ -41,7 +45,7 @@ class RedeemGiftCard extends CartCondition
 
             $manager->validateGiftCard($giftCard);
 
-            $cartSubtotal = Cart::total();
+            $cartSubtotal = $this->calculateGiftUpValue();
 
             $this->giftCardValue = $cartSubtotal > $giftCard->remainingValue
                 ? $giftCard->remainingValue : $cartSubtotal;
@@ -65,5 +69,49 @@ class RedeemGiftCard extends CartCondition
         ];
 
         return [$actions];
+    }
+
+    private function calculateGiftUpValue(): float
+    {
+        $this->conditions = Cart::conditions();
+
+        $cartConditionsTobeAppliedOnGiftup = $this->getConditionsBeforeGiftUp();
+
+        $cartSubtotal = Cart::content()->subtotalWithoutConditions();
+
+        $this->conditions->map(function($cartCondition) use ($cartConditionsTobeAppliedOnGiftup, &$cartSubtotal){
+
+            $filteredCartCondition = $cartConditionsTobeAppliedOnGiftup->first(function($giftupCondition) use ($cartCondition){
+                return $giftupCondition['name'] === $cartCondition->name;
+            });
+
+            if ($filteredCartCondition) {
+                $cartSubtotal += $cartCondition->calculatedValue;
+            }
+        });
+
+        return $cartSubtotal;
+    }
+
+    private function getConditionsBeforeGiftUp(): Collection
+    {
+        $igniterCartSettings = DB::table('extension_settings')
+            ->where('item', 'igniter_cart_settings')
+            ->first();
+
+        $data = json_decode($igniterCartSettings->data, true);
+        $conditions = collect($data['conditions']);
+        $giftupPriority = $conditions->get('giftup')['priority'];
+
+        // Filter conditions with a priority less than 'giftup'
+        $filteredConditions = $conditions->filter(function ($condition) use ($giftupPriority) {
+            return $condition['priority'] < $giftupPriority;
+        });
+
+        // Sort the conditions by priority in ascending order
+        $sortedConditions = $filteredConditions->sortBy('priority');
+
+        // Return the sorted collection
+        return $sortedConditions->values();  // Use values() to reindex the collection
     }
 }
